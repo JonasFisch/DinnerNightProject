c46032b40ca5a373852a54237cd3aa46da751d6b
Jonas Fischer <fischer-jon@t-online.de>
2023-01-09 18:43:33 +0100

    styled invitestatus component (still need improvements)

--- DinnerCookingExpo/src/components/InviteStatus.tsx
+++ DinnerCookingExpo/src/components/InviteStatus.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
 import { UserFirebase } from '../interfaces/FirebaseSchema';
 import { UserImage } from './UserImage';
+import CloseIcon from '../assets/icons/add.svg';
+import { typography } from '../styles/Typography';
 
 type InviteStatusProps = {
   dinnerID: string;
@@ -21,34 +23,79 @@ export const InviteStatus = (props: InviteStatusProps) => {
     return null;
   }
 
+  const revertInvite = () => {
+    console.log('revert invite here!');
+  };
+
   return (
-    <View>
-      <View>
-        <UserImage
-          name={props.participant.name}
-          imageUrl={props.participant.imageUrl}
-        />
-        <Text>{props.participant.name}</Text>
-      </View>
-      <Text>
-        {'invite status - '}
-        <Text style={inviteStyles[inviteState]}>
-          {inviteStateText[inviteState]}
+    <View style={styles.card}>
+      <View style={styles.container}>
+        <View style={styles.userImageContainer}>
+          <UserImage
+            name={props.participant.name}
+            imageUrl={props.participant.imageUrl}
+          />
+          <Text
+            style={[typography.body2, styles.userName]}
+            ellipsizeMode="tail"
+            numberOfLines={1}>
+            {props.participant.name}
+          </Text>
+        </View>
+        <Text style={styles.inviteStatusText}>
+          {'invite status - '}
+          <Text style={[styles.inviteStatus, inviteStyles[inviteState]]}>
+            {inviteStateText[inviteState]}
+          </Text>
         </Text>
-      </Text>
+      </View>
+      <Pressable onPress={() => revertInvite()} style={styles.closeButton}>
+        <CloseIcon />
+      </Pressable>
     </View>
   );
 };
 
+const styles = StyleSheet.create({
+  card: {
+    flex: 1,
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    marginVertical: 16,
+  },
+  container: {
+    flexDirection: 'row',
+  },
+  userImageContainer: {
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: 50,
+  },
+  inviteStatusText: {
+    marginTop: 10,
+    marginLeft: 5,
+  },
+  inviteStatus: {
+    fontWeight: 'bold',
+  },
+  userName: {},
+  closeButton: {
+    height: '100%',
+    aspectRatio: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
 // TODO: use dynamic colors! (waiting for Kathis changes to color pallett!)
 const inviteStyles = StyleSheet.create([
   {
     color: '#888',
   },
   {
-    color: '#f00',
+    color: '#0f0',
   },
   {
-    color: '#0f0',
+    color: '#f00',
   },
 ]);
