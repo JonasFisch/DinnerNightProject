8be5520181843eea50aec31eb70b4bc3011197ee
Kathi <katharina.blass@student.hpi.de>
2023-03-26 19:00:05 +0200

    add more space between userimage and text

--- DinnerCookingExpo/src/components/AvatarList.tsx
+++ DinnerCookingExpo/src/components/AvatarList.tsx
@@ -1,40 +1,40 @@
-import { FlatList, StyleSheet, Text, View } from "react-native"
-import { UserFirebase } from "../interfaces/FirebaseSchema"
-import { spacing } from "../styles/Spacing"
-import { typography } from "../styles/Typography"
-import { UserImage } from "./UserImage"
+import { FlatList, StyleSheet, Text, View } from 'react-native';
+import { UserFirebase } from '../interfaces/FirebaseSchema';
+import { spacing } from '../styles/Spacing';
+import { typography } from '../styles/Typography';
+import { UserImage } from './UserImage';
 import CheckIcon from '../assets/icons/check.svg';
 
 type AvatarListProps = {
-  participants: UserFirebase[],
-  votes: {}
-}
+  participants: UserFirebase[];
+  votes: {};
+};
 
-export const AvatarList = (props: AvatarListProps) => {  
-
+export const AvatarList = (props: AvatarListProps) => {
   const userHasVoted = (userid: string) => {
-    return Object.keys(props.votes).includes(userid)
-  }
+    return Object.keys(props.votes).includes(userid);
+  };
 
-  const renderAvatar = ({ item } : {item: UserFirebase}) => (
-    <View style={[styles.userWrapper, {marginBottom: spacing.m}]}>
-      <UserImage
-        name={item.name}
-        imageUrl={item.imageUrl}
-      />
+  const renderAvatar = ({ item }: { item: UserFirebase }) => (
+    <View style={[styles.userWrapper, { marginBottom: spacing.m }]}>
+      <UserImage name={item.name} imageUrl={item.imageUrl} />
       <Text style={[typography.body2, styles.text]} numberOfLines={1}>
         {item.name}
       </Text>
-      {
-        userHasVoted(item.id) && <CheckIcon />
-      }
+      {userHasVoted(item.id) && <CheckIcon />}
     </View>
   );
 
   return (
-    <FlatList data={props.participants} renderItem={renderAvatar} horizontal style={{flexDirection: "row"}} ItemSeparatorComponent={() => <View style={{width: 40}}></View>}/>
-  ) 
-}
+    <FlatList
+      data={props.participants}
+      renderItem={renderAvatar}
+      horizontal
+      style={{ flexDirection: 'row' }}
+      ItemSeparatorComponent={() => <View style={{ width: 40 }}></View>}
+    />
+  );
+};
 
 const styles = StyleSheet.create({
   userWrapper: {
@@ -44,5 +44,6 @@ const styles = StyleSheet.create({
   },
   text: {
     flex: 1,
+    marginTop: spacing.xs,
   },
-})
+});
