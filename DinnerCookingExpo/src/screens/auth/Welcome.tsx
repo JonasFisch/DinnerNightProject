aa3ee393c742e65b5fa328607223ab46826f23d0
Jonas Fischer <fischer-jon@t-online.de>
2023-03-28 18:08:40 +0200

    fixed welcome screen for ios
    Co-authored-by: Katharina Blaß <KatharinaBlass@users.noreply.github.com>

--- DinnerCookingExpo/src/screens/auth/Welcome.tsx
+++ DinnerCookingExpo/src/screens/auth/Welcome.tsx
@@ -1,46 +1,62 @@
 import React, { useContext, useState } from 'react';
-import { NavigatorIOS, ScrollView, StyleSheet, Text, View } from 'react-native';
+import {
+  Dimensions,
+  NavigatorIOS,
+  Platform,
+  ScrollView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 import { AppButton } from '../../components/Button';
 import { AppButtonType } from '../../interfaces/Button';
 import WelcomeGraphic from '../../assets/graphics/welcome.svg';
 import { Frame } from '../../components/Frame';
 import { typography } from '../../styles/Typography';
 import { spacing } from '../../styles/Spacing';
+import { useNavigation } from '@react-navigation/native';
 
-export const WelcomeScreen = ({ navigation }) => {
+export const WelcomeScreen = () => {
+
+  const navigator = useNavigation()
+
   const navigateLogin = () => {
-    navigation.push('Login');
+    navigator.navigate('Login');
   };
   const navigateRegister = () => {
-    navigation.push('Register');
+    navigator.navigate('Register');
   };
 
   return (
-    <Frame>
-      <View style={styles.imageWrapper}>
-        <WelcomeGraphic height={'100%'} style={styles.distanceBottom} />
-      </View>
-      <View style={styles.textWrapper}>
-        <Text style={[typography.h1, styles.distanceBottom]}>Welcome!</Text>
-        <Text style={[typography.body, styles.distanceBottom]}>
-          Plan cooking parties with your friends and quickly find a recipe that
-          suits everyone's eating preferences
-        </Text>
-        <View style={styles.distanceBottom}>
-          <AppButton
-            title="GET STARTED"
-            type={AppButtonType.primary}
-            style={[styles.buttonDistance]}
-            onPress={navigateRegister}
-          />
-          <AppButton
-            title="I ALREADY HAVE AN ACCOUNT"
-            type={AppButtonType.secondary}
-            onPress={navigateLogin}
+      <Frame>
+        <View style={[styles.imageWrapper]}>
+          <WelcomeGraphic
+            height={'100%'}
+            {...(Platform.OS == 'ios' && { width: 'auto' })}
+            style={styles.distanceBottom}
           />
         </View>
-      </View>
-    </Frame>
+        <View >
+          <Text style={[typography.h2, styles.distanceBottom]}>Welcome!</Text>
+          <Text style={[typography.body, styles.distanceBottom]}>
+            Plan cooking parties with your friends and quickly find a recipe
+            that suits everyone's eating preferences
+          </Text>
+          <View>
+            <AppButton
+              title="GET STARTED"
+              type={AppButtonType.primary}
+              style={[styles.buttonDistance]}
+              onPress={navigateRegister}
+            />
+            <AppButton
+              title="I ALREADY HAVE AN ACCOUNT"
+              type={AppButtonType.secondary}
+              onPress={navigateLogin}
+            />
+          </View>
+        </View>
+      </Frame>
   );
 };
 
@@ -52,5 +68,4 @@ const styles = StyleSheet.create({
     marginBottom: spacing.m,
   },
   imageWrapper: { flex: 1, marginVertical: spacing.xl },
-  textWrapper: { flex: 1 },
 });
